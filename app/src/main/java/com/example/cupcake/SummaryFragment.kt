8c3e6ea6a302a83d878bc83882ef6ecf37b34c93
Oscar Gustavo Moreno Bautista <xascar94@gmail.com>
2023-01-02 03:09:51 -0500

    Shared ViewModel Across Fragments

--- app/src/main/java/com/example/cupcake/SummaryFragment.kt
+++ app/src/main/java/com/example/cupcake/SummaryFragment.kt
@@ -21,11 +21,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.cupcake.databinding.FragmentSummaryBinding
+import com.example.cupcake.model.OrderViewModel
 
 /**
- * [SummaryFragment] contains a summary of the order details with a button to share the order
- * via another app.
+ * [SummaryFragment] contains a summary of the order details with a button to share the order via another app.
  */
 class SummaryFragment : Fragment() {
 
@@ -33,6 +34,9 @@ class SummaryFragment : Fragment() {
     // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
     // when the view hierarchy is attached to the fragment.
     private var binding: FragmentSummaryBinding? = null
+
+    private val sharedViewModel: OrderViewModel by activityViewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,7 +51,9 @@ class SummaryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding?.apply {
-            sendButton.setOnClickListener { sendOrder() }
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = sharedViewModel
+            summaryFragment = this@SummaryFragment
         }
     }
 
