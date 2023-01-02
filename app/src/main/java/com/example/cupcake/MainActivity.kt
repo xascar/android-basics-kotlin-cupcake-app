b38b91f0c3682416cef070db0f06e6463c5b90d1
Oscar Gustavo Moreno Bautista <xascar94@gmail.com>
2023-01-01 20:15:28 -0500

    EI-2 first commit

--- app/src/main/java/com/example/cupcake/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -18,6 +18,6 @@ package com.example.cupcake
 import androidx.appcompat.app.AppCompatActivity
 
 /**
- * Activity for cupcake order flow.
+ * Activity for cupcake order flow
  */
 class MainActivity : AppCompatActivity(R.layout.activity_main)
