3e20c66be2203f5202bd53bcddf0ec8bb3e95cde
Oscar Gustavo Moreno Bautista <xascar94@gmail.com>
2023-01-03 13:49:01 -0500

    Navigation and the backstack

--- app/src/main/java/com/example/cupcake/FlavorFragment.kt
+++ app/src/main/java/com/example/cupcake/FlavorFragment.kt
@@ -77,4 +77,10 @@ class FlavorFragment : Fragment() {
         super.onDestroyView()
         binding = null
     }
+
+    fun cancelOrder() {
+        sharedViewModel.resetOrder()
+        findNavController().navigate(R.id.action_flavorFragment_to_startFragment)
+    }
+
 }
